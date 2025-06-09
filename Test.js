84ff1047520a4b6362d98d8fb9fe51a45ea1f58b
Jaison D Souza <123267719+jsndz@users.noreply.github.com>
2025-06-09 07:54:16 +0000

    AST

--- Test.js
+++ Test.js
@@ -1,1 +1,27 @@
-console.log("HELLOO");
+import { readFileSync, existsSync } from 'fs'; // 'existsSync' is unused
+
+const a = 5;
+let b = 10;
+var c = 15;
+var unused =20;
+
+function add(x, y) {
+  const temp = x + y;
+  return temp;
+}
+
+function unusedFunc(p) {
+  const inner = p * 2;
+  return 42;
+}
+
+try {
+  throw new Error("oops");
+} catch (err) {
+  console.log("Handled");
+}
+
+console.log(a);
+console.log(add(b, c));
+
+
