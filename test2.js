cc3832441f6bc628ec5543365b11d81d1a50d683
Jaison D Souza <123267719+jsndz@users.noreply.github.com>
2025-06-15 09:24:27 +0000

    test:

--- test2.js
+++ test2.js
@@ -1,8 +1,43 @@
-const a = 5;
-let b = 10;
-var c = 15;
-var unused =20;
-var notused=220;
+// Unused variable
+let unusedVar = 42;
 
-console.log(a);
-console.log(add(b, c));
+function calculate(a, b) {
+  // Used variable
+  let result = a + b;
+
+  // Dead code
+  return result;
+  console.log("This will never run");
+}
+
+// Dangerous function
+eval("console.log('Executed via eval')");
+
+// Cyclomatic complexity
+function testComplexity(x) {
+  if (x > 10) {
+    console.log("Greater");
+  } else if (x < 5) {
+    console.log("Smaller");
+  } else {
+    console.log("Between");
+  }
+
+  for (let i = 0; i < 3; i++) {
+    if (i % 2 === 0) {
+      console.log("Even");
+    }
+  }
+
+  switch (x) {
+    case 1:
+      break;
+    case 2:
+      break;
+    default:
+      break;
+  }
+
+  const func = new Function("return 2 + 2;");
+  return func();
+}
