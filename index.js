0bda47bacf653dfe9a05221116d6f96418106cf1
Jaison D Souza <123267719+jsndz@users.noreply.github.com>
2025-06-25 10:52:55 +0000

    testing

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-"HI"
+// Unused variable
+let unusedVar = 42;
+
+function calculate(a, b) {
+  // Used variable
+  let result = a + b;
+
+  // Dead code
+  return result;
+  console.log("This will never run");
+}
+
+// Dangerous function
+eval("console.log('Executed via eval')");
+
+// Cyclomatic complexity
+function testComplexity(x) {
+  if (x > 10) {
+    console.log("Greater");
+  } else if (x < 5) {
+    console.log("Smaller");
+  } else {
+    console.log("Between");
+  }
+
+  for (let i = 0; i < 3; i++) {
+    if (i % 2 === 0) {
+      console.log("Even");
+    }
+  }
+
+  switch (x) {
+    case 1:
+      break;
+    case 2:
+      break;
+    default:
+      break;
+  }
+
+  const func = new Function("return 2 + 2;");
+  return func();
+}
