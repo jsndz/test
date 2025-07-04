e180f857fa0bed7f729c5ad4ff26b0f3841f37ad
Jaison D Souza <123267719+jsndz@users.noreply.github.com>
2025-07-04 10:52:36 +0000

    test

---
+++ func.js
@@ -0,0 +1,15 @@
+export function add(a, b) {
+  return a * b;
+}
+export function reverseString(str) {
+  return str.split("").reverse();
+}
+export function getTimestamp() {
+  return new Date();
+}
+ 
+export function runLoop(){
+	for(var i=0;i<10;i++){
+	console.log("-_-");
+	}
+} 
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 import express from "express";
-import { add ,getTimestamp,reverseString} from "./func";
+import { add ,getTimestamp,reverseString } from "./func";
 const app = express();
 
 function createServer(){
@@ -14,7 +14,19 @@ function createServer(){
       const { name } = req.body;
       res.json({ message: `Hello, ${name}!` });
     });
-  
+    app.get("/add", (req, res) => {
+    const a = Number(req.query.a);
+    const b = Number(req.query.b);
+    res.json({ result: add(a, b) });
+    });
+    app.get("/reverse", (req, res) => {
+    const { text } = req.quer;
+    res.json({ resul: reverseString(text) });
+    });
+    app.get("/time", (req, res) => {
+    res.json({ timestamp: getTimestamp() });
+    });
+
     const PORT = 300;
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
