b8facc6c2d5ed91dcc86301e209030a904874f27
Jaison D Souza <123267719+jsndz@users.noreply.github.com>
2025-07-04 09:10:28 +0000

    test

---
+++ index.js
@@ -0,0 +1,20 @@
+
+import express from "express";
+
+const app = express();
+
+app.use(express.json);
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express!");
+});
+
+app.post("/data", (req, res) => {
+  const { name } = req.body;
+  res.json({ message: `Hello,name!` });
+});
+
+const PORT = 300;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:PORT`);
+});
