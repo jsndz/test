14e49f6256c101e0b4b4591f616aefd69a7e999b
Jaison D Souza <123267719+jsndz@users.noreply.github.com>
2025-07-04 09:19:02 +0000

    test2

--- index.js
+++ index.js
@@ -3,18 +3,23 @@ import express from "express";
 
 const app = express();
 
-app.use(express.json);
+function createServer(){
+    app.use(express.json);
 
-app.get("/", (req, res) => {
-  res.send("Hello from Express!");
-});
+    app.get("/", (req, res) => {
+    res.send("Hello from Express!");
+    });
 
-app.post("/data", (req, res) => {
-  const { name } = req.body;
-  res.json({ message: `Hello,name!` });
-});
+    app.post("/data", (req, res) => {
+    const { name } = req.body;
+    res.json({ message: `Hello,name!` });
+    });
 
-const PORT = 300;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:PORT`);
-});
+    const PORT = 300;
+    app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:PORT`);
+    });
+}
+
+
+createServer()
